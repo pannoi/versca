adb7f62acbcd0e729e9af4eb49d879a4cd2d6005
Anton <anton.poplavnoi@bwt.ee>
2023-04-26 09:42:03 +0300

    add helper: non verssion pattern parser

--- src/helpers/string_parser.py
+++ src/helpers/string_parser.py
@@ -13,3 +13,16 @@ def version_pattern_parser(version: str) -> str:
     deletion_pattern = re.sub(version_pattern, '', version)
 
     return version.replace(deletion_pattern, '')
+
+def non_version_pattern_parser(version: str) -> str:
+    """
+    Parsing string to NON version pattern: 0.0.0
+
+    Parameters:
+        version(str): String to convert into pattern
+    Rerturns:
+        str: Cutted stirng which is NOT matching pattern: 0.0.0
+    """
+    version_pattern = re.compile(r"\d+\.\d+\.\d+")
+        
+    return str(re.sub(version_pattern, '', version))
